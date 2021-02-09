42f9bb5f4279affba3711ac72eec16547d29aebf
Fra0Monta <montalbetti.francesco.norberto@iisgalvanimi.edu.it>
2021-02-09 08:49:50 +0000

    Caricamento

--- mongo/routes/morte.js
+++ mongo/routes/morte.js
@@ -4,7 +4,8 @@ var router = express.Router();
 const MongoClient = require('mongodb').MongoClient; //Importo la libreria mongodb
 
 /* GET users listing. */
-router.get('/morterurale', function (req, res, next) {
+router.get('/morterurale/:nome', function (req, res, next) {
+    y=req.params.nome;
     const uri = 'mongodb+srv://Fiorito:ciao12345@cluster0.2grvr.mongodb.net/DATI?retryWrites=true&w=majority'
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     
@@ -12,14 +13,14 @@ router.get('/morterurale', function (req, res, next) {
         console.log('prova');
         const collection = client.db("DATI").collection("morte_rurale"); //Mi connetto alla collection movies
         // perform actions on the collection object
-        collection.find().limit(10).toArray((err, result) => {
+        collection.find({"Country Name":y}).limit(10).toArray((err, result) => {
             if (err) console.log(err.message); //Se c'è qualche errore lo stampo
-            else console.log(result);
+            else res.send(result);
             //client.close(); //Quando ho terminato la find chiudo la sessione con il db
         }); //Eseguo la query e passo una funzione di callback
             
     });
-    res.send(result);
+    
 });
 
 module.exports = router;
--- mongo/routes/popolazionerurale.js
+++ mongo/routes/popolazionerurale.js
@@ -4,22 +4,22 @@ var router = express.Router();
 const MongoClient = require('mongodb').MongoClient; //Importo la libreria mongodb
 
 /* GET users listing. */
-router.get('/popolazionerurale', function (req, res, next) {
+router.get('/popolazionerurale/:nome', function (req, res, next) {
+    y=req.params.nome;
     const uri = 'mongodb+srv://Fiorito:ciao12345@cluster0.2grvr.mongodb.net/DATI?retryWrites=true&w=majority'
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    
     client.connect(err => {
         console.log('prova');
         const collection = client.db("DATI").collection("popolazione_rurale"); //Mi connetto alla collection movies
         // perform actions on the collection object
-        collection.find().limit(10).toArray((err, result) => {
+        collection.find({"Country Name":y}).limit(10).toArray((err, result) => {
             if (err) console.log(err.message); //Se c'è qualche errore lo stampo
-            else console.log(result);
+            else res.send(result);
             //client.close(); //Quando ho terminato la find chiudo la sessione con il db
         }); //Eseguo la query e passo una funzione di callback
             
     });
-    res.send(result);
+    
 });
 
 module.exports = router;
